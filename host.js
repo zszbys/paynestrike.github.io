02e2a0dd73bd29d72812b7f88b684cc866affd6e
paynestrike <paynestrike@gmail.com>
2016-09-13 15:37:23 +0800

    test non exsit api

--- host.js
+++ host.js
@@ -1,20 +1,31 @@
 (function() {
 
     document.getElementById("hostBtn").onclick = function(e) {
-        Seismic.callApi('getUserInfo', function(err, info){
+        Seismic.callApi('getUserInfo', function(err, res){
+            if(err){
+                alert("Error : "+err.toString());
+                return;
+            }
+
+            console.log(res);
+            var resultDom = document.getElementById('result');
+            var temp = '<table>';
+            temp += '<tr><th>'+res.name+'<th><td>'+res.profile[0]+'</td></tr>';
+            temp += '<tr><th>'+res.name+'<th><td>'+res.profile[1]+'</td></tr>';
+            temp += '</table>';
+            resultDom.innerHTML = temp;
+        })
+    };
+
+    document.getElementById("errorBtn").onclick = function(e) {
+        Seismic.callApi('nonExsitApi', function(err, res){
             if(err){
                 alert("Error : "+err.toString());
                 return;
             }
 
             console.log(info);
-            var resultDom = document.getElementById('result');
-            var temp = '<table>';
-            temp += '<tr><th>'+info.name+'<th><td>'+info.profile[0]+'</td></tr>';
-            temp += '<tr><th>'+info.name+'<th><td>'+info.profile[1]+'</td></tr>';
-            temp += '</table>';
-            resultDom.innerHTML = temp;
         })
-    }
+    };
 
 }())
--- messagehost.js
+++ messagehost.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
     var message = {};
     message.host = "https://c.na35.visual.force.com";
     message.stack = [];
@@ -7,80 +7,82 @@
     child.domain = "https://paynestrike.github.io";
     child.iframe = document.getElementById('receiver').contentWindow;
 
-    function postToIframe(args, callback){
-        var id = (new Date()).getTime()+'';
-        var stack = {id:id, callback:callback};
+
+    function isString(str) {
+        return Object.prototype.toString.call(str) === "[object String]";
+    }
+
+    function isFunc(func) {
+        return Object.prototype.toString.call(func) === "[object Function]";
+    }
+
+    function isObject(obj) {
+        return Object.prototype.toString.call(obj) === "[object Otring]";
+    }
+
+    function postToIframe(args, callback) {
+        var id = (new Date()).getTime() + '';
+        var stack = {
+            id: id,
+            callback: callback
+        };
         message.stack.push(stack);
         var data = {
-            data:args,
-            id:id
+            data: args,
+            id: id
         }
         _postToIframe(JSON.stringify(data), child.domain);
     }
 
-    function _postToIframe(data, host){
-        try{
+    function _postToIframe(data, host) {
+        try {
             child.iframe.postMessage(data, host);
-        }catch(e){
-            runCallback(e, data);
-            alert("Erroe : " + e.name + "\nmessage : " + e.message);
+        } catch (e) {
+            throw "Error : " + e.name + "\nmessage : " + e.message;
         }
     }
 
-    function getStack(id){
+    function getStack(id) {
         for (var i = message.stack.length - 1; i >= 0; i--) {
-            if(message.stack[i].id === id){
+            if (message.stack[i].id === id) {
                 return message.stack[i].callback;
             }
         }
     }
 
-    function removeStack(id){
+    function removeStack(id) {
         var stackIndex = null;
         var stack = message.stack;
         var len = stack.length;
         for (var i = len - 1; i >= 0; i--) {
-            if(stack[i].id === id){
+            if (stack[i].id === id) {
                 stackIndex = i;
                 break;
             }
         }
 
-        stack.splice(i,1);
+        stack.splice(i, 1);
     }
 
-    function runCallback(data){
+    function runCallback(data) {
         var id = data.id;
         var callback = getStack(id);
-        if (Object.prototype.toString.call(callback) === "[object Function]"){
+        if (Object.prototype.toString.call(callback) === "[object Function]") {
             callback(data.error, data.res);
             removeStack(id);
         }
     }
 
-    window.addEventListener("message", function(e){
+    window.addEventListener("message", function(e) {
         if (e.origin !== child.domain) {
-              return; 
+            return;
         }
 
         var data = JSON.parse(e.data);
-        
+
         runCallback(data);
-              
+
     });
-
-    function isString(str){
-        return Object.prototype.toString.call(str) === "[object String]";
-    }
-
-    function isFunc(func){
-        return Object.prototype.toString.call(func) === "[object Function]";
-    }
-
-    function isObject(obj){
-        return Object.prototype.toString.call(obj) === "[object Otring]";
-    }
-
 
     var seismic = {};
 
@@ -88,18 +90,20 @@
      * apiName non option
      * postData optional
      * callback optional
-    */
+     */
 
-    seismic.callApi = function(apiName, postData, callback){
+    seismic.callApi = function(apiName, postData, callback) {
         if (!apiName) {
             throw 'require apiName';
         }
         if (!isString(apiName)) {
             throw 'apiName must be a string';
         }
-        var data = {api:apiName};
+        var data = {
+            api: apiName
+        };
         var cb = null;
-        if (isObject(postData)){
+        if (isObject(postData)) {
             data.args = postData;
         }
         if (isFunc(postData) && !callback) {
