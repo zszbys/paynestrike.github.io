409ba781bb68ec640747bdd7820b1c60fca24ac7
paynestrike <paynestrike@gmail.com>
2016-09-13 14:10:58 +0800

    fwefwefwefwefbcvnvbnvbnvbnvb

--- api.js
+++ api.js
@@ -5,7 +5,7 @@
         setTimeout(function(){
             var userInfo = {name:'dexter',profile:['default', 'sales']};
             callback(null, userInfo);
-        })
+        },3000)
     }
 
     window.seismicapi = api;
