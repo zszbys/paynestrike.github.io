0a78c6434c4623bedb45cc5e179b59e07fc8bd0b
paynestrike <paynestrike@gmail.com>
2016-09-13 16:09:14 +0800

    fix bug

--- hostApi.js
+++ hostApi.js
@@ -1,7 +1,7 @@
 (function(){
     var api = {};
 
-    api.showErrorMessage(message){
+    api.showErrorMessage = function(message){
         var errorDom = document.getElementById('error');
         var temp = '<div style="background-color:pink; color:red ">'+message+'</div>';
         errorDom.innerHTML = temp;
