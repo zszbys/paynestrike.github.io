0ac56ae5f897acf5e4a063e0aad084c9b7ceea90
paynestrike <paynestrike@gmail.com>
2016-09-13 15:29:35 +0800

    add  test ui

--- host.js
+++ host.js
@@ -7,7 +7,13 @@
                 return;
             }
 
-            alert("Success : "+info.toString());
+            console.log(info);
+            var resultDom = document.getElementById('result');
+            var temp = '<table>';
+            temp += '<tr><th>'+info.name+'<th><td>'+info.profile[0]+'</td></tr>';
+            temp += '<tr><th>'+info.name+'<th><td>'+info.profile[1]+'</td></tr>';
+            temp += '</table>';
+            resultDom.innerHTML = temp;
         })
     }
 
