49a2c0e3546e1cedf7d2c9fc305149f5912b17be
paynestrike <paynestrike@gmail.com>
2016-09-13 17:50:15 +0800

    add api to host

--- SalesforceApi.js
+++ SalesforceApi.js
@@ -5,7 +5,20 @@
         var errorDom = document.getElementById('error');
         var temp = '<div style="color:red ">'+message+'</div>';
         errorDom.innerHTML = temp;
-        errorDom.style.display = "block";
+        errorDom.style.display = 'block';
+    }
+
+    api.confirm = function(message, callback){
+        var confirmDailog = document.getElementById('confirm');
+        confirmDailog.display = 'block';
+        document.getElementById('confirm-cancel').onclick = function(event){
+            confirmDailog.display = 'none';
+            callback(null, {text:'cancel'})
+        }
+        document.getElementById('confirm-ok').onclick = function(event){
+            confirmDailog.display = 'none';
+            callback(null, {text:'confirm'})
+        }
     }
 
     window.HostApi = api;
